94b6627bb6017bae9b3b3b18f250d38c9911097d
nanjapapa <bzhangusc@live.com>
2022-03-04 21:30:45 -0800

    #33 cleanup templates

--- tools/templates/bigChain/src/main/python/stage1/input.py
+++
@@ -1,7 +0,0 @@
-import smv
-
-__all__ = ['EmploymentByState']
-
-class input(smv.SmvCsvFile):
-    def path(self):
-        return "employment/CB1200CZ11.csv"
--- tools/templates/enterprise/src/main/python/stage2/inputdata.py
+++
@@ -1,4 +0,0 @@
-from smv import SmvModuleLink
-from stage1 import employment as emp
-
-EmploymentByStateLink = SmvModuleLink(emp.EmploymentByState)
