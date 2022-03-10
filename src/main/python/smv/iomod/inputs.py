815c8382f59d37dddc1ec476b70dfe6dc4929bdf
nanjapapa <bzhangusc@live.com>
2022-03-10 13:56:59 -0800

    fixed bug in PERMISSIVE mode

--- src/main/python/smv/iomod/inputs.py
+++ src/main/python/smv/iomod/inputs.py
@@ -294,12 +294,11 @@ class WithCsvParser(WithSmvSchema, SmvInput):
             # The following will extract the clean records and return, but output the
             # corrupted records to a file
             # column name "_corrupt_record" is defined in buildCsvIO() of SmvApp
-            raw_csv.cache()
+            raw_csv.cache()         # need to cache it after spark 2.3 
             clean_df = raw_csv.where(F.col("_corrupt_record").isNull()).drop("_corrupt_record")
             corrupted = raw_csv.where(F.col("_corrupt_record").isNotNull()).select("_corrupt_record")
             path = self.smvApp.output_path_from_base("{}_corrupted".format(self.fqn()), "csv")
             SmvCsvOnHdfsIoStrategy(self.smvApp, path).write(corrupted)
-            raw_csv.unpersist()
             return clean_df
         else:
             return raw_csv
